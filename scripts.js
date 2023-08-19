111c97e09bd3c2f65b7dce09889e0064e749dc90
y1chu <78563674+y1chu@users.noreply.github.com>
2023-08-19 04:34:56 -0700

    Fixed animation for Experience

--- scripts.js
+++ scripts.js
@@ -1,15 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-    checkFadeInSection();
-
     function isElementInViewport(el) {
         const rect = el.getBoundingClientRect();
-        return (rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.bottom >= 0);
+        return (rect.top <= (window.innerHeight || document.documentElement.clientHeight) && rect.bottom >= 0);
     }
 
     function checkFadeInSection() {
-        const sections = document.querySelectorAll('.fade-in-section');
-        sections.forEach(section => {
+        document.querySelectorAll('.fade-in-section').forEach(section => {
             if (isElementInViewport(section)) {
                 section.style.opacity = 1;
             }
@@ -22,11 +18,26 @@ document.addEventListener("DOMContentLoaded", function () {
         targetSection.scrollIntoView({behavior: 'smooth'});
     }
 
-    // Event listeners for smooth scrolling
     document.querySelectorAll('.nav-link, .icons-container .section-icon').forEach(el => {
         el.addEventListener('click', (event) => smoothScrolling(el, event));
     });
 
+    // Intersection Observer
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('is-visible');
+            } else {
+                entry.target.classList.remove('is-visible');
+            }
+        });
+    });
+
+    document.querySelectorAll('.fade-in-section').forEach(section => {
+        observer.observe(section);
+    });
+
+    // Scrolling logic
     let lastSectionName = "Home";
     let isUsingFirstText = true;
 
@@ -34,49 +45,44 @@ document.addEventListener("DOMContentLoaded", function () {
         checkFadeInSection();
         const scrollPercentage = (window.scrollY) / (document.documentElement.scrollHeight - window.innerHeight);
 
-        // Progress bar update
         const progressBar = document.querySelector('.progress-fill');
         progressBar.style.width = `${scrollPercentage * 100}%`;
 
-        const sections = ['#home', '#about', '#experience', '#projects', '#contact'];
-        let currentSection = sections[0];
-
-        sections.forEach(section => {
+        let currentSection = '#home';
+        ['#home', '#about', '#experience', '#projects', '#contact'].forEach(section => {
             const elem = document.querySelector(section);
-            if (elem.getBoundingClientRect().top < window.innerHeight / 2) {
+            if (elem && elem.getBoundingClientRect().top < window.innerHeight / 2) {
                 currentSection = section;
             }
         });
 
-        // Update icons
         document.querySelectorAll('.debuff-icon, .section-icon').forEach(icon => {
             icon.classList.toggle('active', icon.getAttribute('data-section') === currentSection);
         });
 
         const newSectionName = currentSection.slice(1).charAt(0).toUpperCase() + currentSection.slice(2);
 
-        // Update section text
         if (lastSectionName !== newSectionName) {
             const sectionTextElement = document.getElementById(isUsingFirstText ? 'current-section-1' : 'current-section-2');
             const otherSectionTextElement = document.getElementById(isUsingFirstText ? 'current-section-2' : 'current-section-1');
 
-            sectionTextElement.style.opacity = 0;
-            otherSectionTextElement.textContent = newSectionName;
-            otherSectionTextElement.style.opacity = 1;
+            if (sectionTextElement) {
+                sectionTextElement.style.opacity = 0;
+            }
+
+            if (otherSectionTextElement) {
+                otherSectionTextElement.textContent = newSectionName;
+                otherSectionTextElement.style.opacity = 1;
+            }
 
             isUsingFirstText = !isUsingFirstText;
             lastSectionName = newSectionName;
         }
 
-        // Update gradient
         const r = Math.floor(255 - (scrollPercentage * (255 - 212)));
         const g = Math.floor(112 - (scrollPercentage * 112));
         const b = Math.floor(67 - (scrollPercentage * 67));
-
         document.querySelector(".content-wrapper").style.background = `linear-gradient(to bottom, #ff7043, rgb(${r},${g},${b}))`;
-
-        // Check fade-in sections
-        checkFadeInSection();
     });
 
     // Homepage fade-in
@@ -85,10 +91,12 @@ document.addEventListener("DOMContentLoaded", function () {
         homeSection.style.opacity = 0;
         homeSection.style.transition = 'opacity 1s ease-in-out';
         setTimeout(() => {
-            homeSection.style.opacity = 1;
+            if (homeSection) {
+                homeSection.style.opacity = 1;
+            }
         }, 100);
     }
-    // Particles.js configuration for fiery background effect
+
     particlesJS('particles-js', {
         particles: {
             number: {
@@ -193,131 +201,21 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
-
-document.addEventListener("DOMContentLoaded", function () {
-    // Get all the sections to be observed
-    const sections = document.querySelectorAll('.fade-in-section');
-
-    const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('is-visible');
-            } else {
-                entry.target.classList.remove('is-visible');
-            }
-        });
-    });
-
-    // Observe each section
-    sections.forEach(section => {
-        observer.observe(section);
-    });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-    checkFadeInSection();
-
-    function isElementInViewport(el) {
-        const rect = el.getBoundingClientRect();
-        return (rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.bottom >= 0);
-    }
-
-    function checkFadeInSection() {
-        const sections = document.querySelectorAll('.fade-in-section');
-        sections.forEach(section => {
-            if (isElementInViewport(section)) {
-                section.style.opacity = 1;
-            }
-        });
-    }
-
-    function smoothScrolling(element, event) {
-        event.preventDefault();
-        const targetSection = document.querySelector(element.getAttribute(event.target.hasAttribute('href') ? 'href' : 'data-section'));
-        targetSection.scrollIntoView({behavior: 'smooth'});
-    }
-
-    // Event listeners for smooth scrolling
-    document.querySelectorAll('.nav-link, .icons-container .section-icon').forEach(el => {
-        el.addEventListener('click', (event) => smoothScrolling(el, event));
-    });
-
-    let lastSectionName = "Home";
-    let isUsingFirstText = true;
-
-    window.addEventListener("scroll", function () {
-        checkFadeInSection();
-        const scrollPercentage = (window.scrollY) / (document.documentElement.scrollHeight - window.innerHeight);
-
-        // Progress bar update
-        const progressBar = document.querySelector('.progress-fill');
-        progressBar.style.width = `${scrollPercentage * 100}%`;
-
-        const sections = ['#home', '#about', '#experience', '#projects', '#contact'];
-        let currentSection = sections[0];
-
-        sections.forEach(section => {
-            const elem = document.querySelector(section);
-            if (elem && elem.getBoundingClientRect().top < window.innerHeight / 2) {
-                currentSection = section;
-            }
-        });
-
-        // Update icons
-        document.querySelectorAll('.debuff-icon, .section-icon').forEach(icon => {
-            icon.classList.toggle('active', icon.getAttribute('data-section') === currentSection);
-        });
-
-        const newSectionName = currentSection.slice(1).charAt(0).toUpperCase() + currentSection.slice(2);
-
-        // Update section text
-        if (lastSectionName !== newSectionName) {
-            const sectionTextElement = document.getElementById(isUsingFirstText ? 'current-section-1' : 'current-section-2');
-            const otherSectionTextElement = document.getElementById(isUsingFirstText ? 'current-section-2' : 'current-section-1');
-
-            if (sectionTextElement) {
-                sectionTextElement.style.opacity = 0;
-            }
-
-            if (otherSectionTextElement) {
-                otherSectionTextElement.textContent = newSectionName;
-                otherSectionTextElement.style.opacity = 1;
-            }
-
-            isUsingFirstText = !isUsingFirstText;
-            lastSectionName = newSectionName;
-        }
-
-        // Update gradient
-        const r = Math.floor(255 - (scrollPercentage * (255 - 212)));
-        const g = Math.floor(112 - (scrollPercentage * 112));
-        const b = Math.floor(67 - (scrollPercentage * 67));
-
-        document.querySelector(".content-wrapper").style.background = `linear-gradient(to bottom, #ff7043, rgb(${r},${g},${b}))`;
-
-        // Check fade-in sections
-        checkFadeInSection();
-    });
-
-    // Homepage fade-in
-    const homeSection = document.querySelector('#home');
-    if (homeSection) {
-        homeSection.style.opacity = 0;
-        homeSection.style.transition = 'opacity 1s ease-in-out';
-        setTimeout(() => {
-            if (homeSection) {
-                homeSection.style.opacity = 1;
-            }
-        }, 100);
-    }
-});
-
-// Function to update the job description
 function updateJobDescription(jobKey) {
     const descriptionDiv = document.getElementById('job-description');
     if (descriptionDiv && jobDescriptions[jobKey]) {
-        descriptionDiv.innerHTML = jobDescriptions[jobKey];
+        // Hide the descriptionDiv first
+        descriptionDiv.style.opacity = '0';
+        descriptionDiv.style.transform = 'translateX(10%)';
+
+        setTimeout(() => {
+            // Change its content
+            descriptionDiv.innerHTML = jobDescriptions[jobKey];
+            // Show the div smoothly
+            descriptionDiv.style.opacity = '1';
+            descriptionDiv.style.transform = 'translateX(0)';
+            descriptionDiv.classList.add('showing');
+        }, 300);  // This delay should match the transition duration in CSS
     }
 }
 
@@ -348,7 +246,44 @@ const jobDescriptions = {
 
 // Add event listeners for job list items
 document.querySelectorAll('#job-list li').forEach(li => {
-    li.addEventListener('click', function () {
+    li.addEventListener('click', function() {
         updateJobDescription(li.getAttribute('data-job'));
     });
+});
+
+// Function to update the highlighted experience
+function updateHighlightedExperience() {
+    const jobList = document.querySelector("#job-list");
+    const experiences = jobList.querySelectorAll("li");
+
+    experiences.forEach(exp => {
+        exp.classList.remove("selected-experience");
+    });
+
+    let selectedExperience = jobList.querySelector("[data-job='unity']") || experiences[0];
+    selectedExperience.classList.add("selected-experience");
+}
+
+// Event listener to change highlighted experience on click
+document.querySelector("#job-list").addEventListener("click", function(event) {
+    if (event.target && event.target.nodeName === "LI") {
+        const jobList = document.querySelector("#job-list");
+        const experiences = jobList.querySelectorAll("li");
+
+        experiences.forEach(exp => {
+            exp.classList.remove("selected-experience");
+        });
+
+        event.target.classList.add("selected-experience");
+    }
+});
+
+// Call the function on page load to set the default highlighted experience
+document.addEventListener("DOMContentLoaded", function() {
+    updateHighlightedExperience();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    updateHighlightedExperience();
+    updateJobDescription('unity'); // This will set the default description to "Play Meow"
 });
